5e86e85bced5e46b38fd90452f7c94bcdebc3d21
Huzky <iche.calunsag.coc@phinmaed.com>
2026-02-09 18:38:18 +0800

    updated

--- admin/audit.php
+++ admin/audit.php
@@ -7,7 +7,7 @@ include '../api/connection/db_config.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Audit Logs - Philippians CDO</title>
-    <link rel="stylesheet" href="../css/font-awesome.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link rel="stylesheet" href="../css/audit.css">
      <script src="../js/audit.js" defer></script>
 </head>
